17f8b2a05b07a3d252f91438fe173eecff9d0087
Nayla Pereira <naylapereira06@gmail.com>
2026-06-23 01:33:39 -0300

    agregue la generacion de colores

--- js/app.js
+++ js/app.js
@@ -1,8 +1,49 @@
-const generateBtn =
-document.getElementById("generate-btn");
+const generateBtn = document.getElementById("generate-btn");
+const paletteContainer = document.getElementById("palette-container");
 
-generateBtn.addEventListener("click", () => {
+function randomColor() {
 
-    console.log("Generando paleta...");
+    const letters = "0123456789ABCDEF";
 
-});
+    let color = "#";
+
+    for(let i = 0; i < 6; i++) {
+
+        color += letters[Math.floor(Math.random() * 16)];
+
+    }
+
+    return color;
+}
+
+function generatePalette() {
+
+    paletteContainer.innerHTML = "";
+
+    for(let i = 0; i < 6; i++) {
+
+        const card = document.createElement("div");
+
+        const color = randomColor();
+
+        card.classList.add("color-card");
+
+        card.innerHTML = `
+            <div
+                class="color-preview"
+                style="background:${color}">
+            </div>
+
+            <p class="color-code">
+                ${color}
+            </p>
+        `;
+
+        paletteContainer.appendChild(card);
+    }
+}
+
+generateBtn.addEventListener(
+    "click",
+    generatePalette
+);
