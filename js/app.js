5431e0c7b7f7d6b26c1118f7b5d6157f62c22582
Nayla Pereira <naylapereira06@gmail.com>
2026-06-23 01:51:55 -0300

    mejore la generacion de colores

--- js/app.js
+++ js/app.js
@@ -4,17 +4,52 @@ const paletteSize = document.getElementById("palette-size");
 
 function randomColor() {
 
-    const letters = "0123456789ABCDEF";
+    const hue = Math.floor(Math.random() * 360);
 
-    let color = "#";
+    return `hsl(${hue}, 70%, 75%)`;
+}
 
-    for(let i = 0; i < 6; i++) {
+function hslToHex(h, s, l) {
 
-        color += letters[Math.floor(Math.random() * 16)];
+    s /= 100;
+    l /= 100;
 
+    const c = (1 - Math.abs(2 * l - 1)) * s;
+    const x = c * (1 - Math.abs((h / 60) % 2 - 1));
+    const m = l - c / 2;
+
+    let r = 0;
+    let g = 0;
+    let b = 0;
+
+    if (h < 60) {
+        r = c;
+        g = x;
+    } else if (h < 120) {
+        r = x;
+        g = c;
+    } else if (h < 180) {
+        g = c;
+        b = x;
+    } else if (h < 240) {
+        g = x;
+        b = c;
+    } else if (h < 300) {
+        r = x;
+        b = c;
+    } else {
+        r = c;
+        b = x;
     }
 
-    return color;
+    r = Math.round((r + m) * 255);
+    g = Math.round((g + m) * 255);
+    b = Math.round((b + m) * 255);
+
+    return "#" +
+        r.toString(16).padStart(2, "0") +
+        g.toString(16).padStart(2, "0") +
+        b.toString(16).padStart(2, "0");
 }
 
 function generatePalette() {
@@ -40,6 +75,12 @@ function generatePalette() {
 
         const color = randomColor();
 
+        const hue =
+            Number(color.match(/\d+/)[0]);
+
+        const hexColor =
+            hslToHex(hue, 70, 75);
+
         card.classList.add("color-card");
 
         card.innerHTML = `
@@ -49,7 +90,7 @@ function generatePalette() {
             </div>
 
             <p class="color-code">
-                ${color}
+                ${hexColor}
             </p>
         `;
 
